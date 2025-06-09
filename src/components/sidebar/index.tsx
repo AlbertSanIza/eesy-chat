a14df329734e91b07d8f64379e68b876880dc8e5
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-09 13:42:59 -0700

    feat: enhance AppSidebar with header and user avatar for improved navigation

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,14 +1,31 @@
-import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarHeader } from '@/components/ui/sidebar'
+import { Link } from '@tanstack/react-router'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenuButton, SidebarRail } from '@/components/ui/sidebar'
 
 export function AppSidebar() {
     return (
         <Sidebar>
-            <SidebarHeader />
-            <SidebarContent>
-                <SidebarGroup />
-                <SidebarGroup />
-            </SidebarContent>
-            <SidebarFooter />
+            <SidebarHeader>
+                <h1 className="my-1 border-b border-transparent text-center text-xl font-bold">TheOpenChat</h1>
+                <Button asChild>
+                    <Link to="/">New Chat</Link>
+                </Button>
+            </SidebarHeader>
+            <SidebarContent />
+            <SidebarFooter>
+                <SidebarMenuButton size="lg" asChild>
+                    <Link to="/">
+                        <Avatar>
+                            <AvatarImage src="https://github.com/albertsaniza.png" alt="@albertsaniza" />
+                            <AvatarFallback>AS</AvatarFallback>
+                        </Avatar>
+                        Albert Sanchez
+                    </Link>
+                </SidebarMenuButton>
+            </SidebarFooter>
+            <SidebarRail />
         </Sidebar>
     )
 }
