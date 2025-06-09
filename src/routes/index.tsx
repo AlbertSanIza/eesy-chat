1e4b5d8e31ddbd71fee0906bc977d965a6d99a9b
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-09 13:00:06 -0700

    feat: integrate ThemeToggle component into the main route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { ThemeToggle } from '@/components/theme-toggle'
+
 export const Route = createFileRoute('/')({
     component: RouteComponent
 })
 
 function RouteComponent() {
-    return <div>Hello "/"!</div>
+    return (
+        <div>
+            <ThemeToggle />
+        </div>
+    )
 }
