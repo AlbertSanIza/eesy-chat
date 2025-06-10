1025a25ad3c15cb6047c6d79977a5353c428dd9d
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-09 23:29:47 -0700

    feat: add AppSidebarTrigger component to enhance sidebar functionality

---
+++ src/components/sidebar/trigger.tsx
@@ -0,0 +1,28 @@
+import { Link } from '@tanstack/react-router'
+import { PlusIcon } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { SidebarTrigger, useSidebar } from '@/components/ui/sidebar'
+import { cn } from '@/lib/utils'
+
+export function AppSidebarTrigger() {
+    const { open, isMobile } = useSidebar()
+
+    return (
+        <div
+            className={cn(
+                'fixed top-0 left-0 z-10 gap-2 rounded-br-lg border-r border-b bg-sidebar transition-all duration-200 ease-linear',
+                open && !isMobile ? 'border-transparent p-0 pt-2 pl-2' : 'p-2'
+            )}
+        >
+            <SidebarTrigger className="size-9" size="icon" variant="ghost" />
+            {(!open || isMobile) && (
+                <Button size="icon" variant="ghost" asChild>
+                    <Link to="/">
+                        <PlusIcon className="size-5" />
+                    </Link>
+                </Button>
+            )}
+        </div>
+    )
+}
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 
 import { AppSidebar } from '@/components/sidebar'
+import { AppSidebarTrigger } from '@/components/sidebar/trigger'
 import { ThemeProvider } from '@/components/theme-provider'
 import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar'
 
@@ -13,6 +14,7 @@ function RootComponent() {
         <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
             <SidebarProvider>
                 <AppSidebar />
+                <AppSidebarTrigger />
                 <SidebarInset>
                     <Outlet />
                 </SidebarInset>
