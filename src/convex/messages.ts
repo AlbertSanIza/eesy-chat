766d6115abf4f75ed9c60802075053e9cb0a69b1
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-10 15:26:17 -0700

    feat: enhance message handling with streaming integration and history retrieval

--- src/convex/messages.ts
+++ src/convex/messages.ts
@@ -1,6 +1,7 @@
 import { v } from 'convex/values'
 
-import { internalMutation, mutation, query } from './_generated/server'
+import { StreamId } from '@convex-dev/persistent-text-streaming'
+import { internalMutation, internalQuery, mutation, query } from './_generated/server'
 import { streamingComponent } from './streaming'
 
 export const findAll = query({
@@ -29,5 +30,72 @@ export const removeAll = internalMutation({
             .filter((q) => q.eq(q.field('threadId'), threadId))
             .collect()
         await Promise.all(messages.map((message) => ctx.db.delete(message._id)))
+    }
+})
+
+export const listMessages = query({
+    args: {},
+    handler: async (ctx) => {
+        return await ctx.db.query('messages').collect()
+    }
+})
+
+export const clearMessages = mutation({
+    args: {},
+    handler: async (ctx) => {
+        const chats = await ctx.db.query('messages').collect()
+        await Promise.all(chats.map((chat) => ctx.db.delete(chat._id)))
+    }
+})
+
+export const sendMessage = mutation({
+    args: {
+        threadId: v.id('threads'),
+        prompt: v.string()
+    },
+    handler: async (ctx, args) => {
+        const streamId = await streamingComponent.createStream(ctx)
+        const chatId = await ctx.db.insert('messages', {
+            threadId: args.threadId,
+            prompt: args.prompt,
+            streamId
+        })
+        return chatId
+    }
+})
+
+export const getHistory = internalQuery({
+    args: {},
+    handler: async (ctx) => {
+        // Grab all the user messages
+        const allMessages = await ctx.db.query('messages').collect()
+
+        // Lets join the user messages with the assistant messages
+        const joinedResponses = await Promise.all(
+            allMessages.map(async (userMessage) => {
+                return {
+                    userMessage,
+                    responseMessage: await streamingComponent.getStreamBody(ctx, userMessage.streamId as StreamId)
+                }
+            })
+        )
+
+        return joinedResponses.flatMap((joined) => {
+            const user = {
+                role: 'user' as const,
+                content: joined.userMessage.prompt
+            }
+
+            const assistant = {
+                role: 'assistant' as const,
+                content: joined.responseMessage.text
+            }
+
+            // If the assistant message is empty, its probably because we have not
+            // started streaming yet so lets not include it in the history
+            if (!assistant.content) return [user]
+
+            return [user, assistant]
+        })
     }
 })
