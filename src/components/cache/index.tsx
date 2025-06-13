4044eb4c12216a518ba0dfc9be4468657336210d
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-13 13:53:23 -0700

    Add Cache and ThreadsCache components for thread management and display

--- src/components/cache/index.tsx
+++ src/components/cache/index.tsx
@@ -1,8 +1,14 @@
+import { ThreadsCache } from '@/components/cache/threads'
 import { useCachedThreads } from '@/hooks/use-cached-threads'
 import { useCachedUser } from '@/hooks/use-cached-user'
 
 export function Cache() {
-    useCachedThreads()
     useCachedUser()
-    return null
+    useCachedThreads()
+
+    return (
+        <>
+            <ThreadsCache />
+        </>
+    )
 }
--- src/routes/$threadId.tsx
+++ src/routes/$threadId.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute, useParams } from '@tanstack/react-router'
+import { useQuery } from 'convex/react'
 
 import { Messages } from '@/components/messages'
+import { api } from '@/convex/_generated/api'
 import type { Id } from '@/convex/_generated/dataModel'
 
 export const Route = createFileRoute('/$threadId')({
@@ -9,10 +11,12 @@ export const Route = createFileRoute('/$threadId')({
 
 function RouteComponent() {
     const { threadId } = useParams({ from: Route.fullPath })
+    const thread = useQuery(api.threads.findOne, { id: threadId as Id<'threads'> })
 
     return (
         <div className="w-full pt-14 pb-33 sm:pt-8">
             <div className="mx-auto flex max-w-5xl flex-col gap-6 px-6 sm:px-14">
+                {JSON.stringify(thread)}
                 <Messages threadId={threadId as Id<'threads'>} />
             </div>
         </div>
