793cf06ad295e1c486b73d49d81934a9a8136890
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-13 09:51:41 -0700

    Refactor AppSidebar component to streamline user display logic and remove unused Authenticated and AuthLoading components

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,6 +1,6 @@
-import { SignInButton, UserButton } from '@clerk/clerk-react'
+import { SignInButton, UserButton, useUser } from '@clerk/clerk-react'
 import { Link } from '@tanstack/react-router'
-import { Authenticated, AuthLoading, Unauthenticated } from 'convex/react'
+import { Unauthenticated } from 'convex/react'
 
 import { AppSidebarContent } from '@/components/sidebar/content'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
@@ -9,9 +9,11 @@ import { Input } from '@/components/ui/input'
 import { Sidebar, SidebarFooter, SidebarHeader, SidebarMenuButton } from '@/components/ui/sidebar'
 import { Skeleton } from '@/components/ui/skeleton'
 import { useStore } from '@/lib/store'
+import { cn } from '@/lib/utils'
 
 export function AppSidebar() {
     const { user } = useStore()
+    const { isLoaded, isSignedIn } = useUser()
 
     return (
         <Sidebar variant="floating">
@@ -23,35 +25,36 @@ export function AppSidebar() {
                 <Input />
             </SidebarHeader>
             <AppSidebarContent />
-            <SidebarFooter>
-                <Authenticated>
-                    <div className="clerk-user-button">
-                        <UserButton showName />
-                    </div>
-                </Authenticated>
+            <SidebarFooter className="relative">
+                <div className={cn('clerk-user-button transition-opacity', (!isLoaded || !isSignedIn) && 'opacity-0')}>
+                    <UserButton showName />
+                </div>
+                <div
+                    className={cn(
+                        'pointer-events-none absolute bottom-[8px] flex items-center gap-2 p-2 transition-opacity',
+                        isLoaded || isSignedIn ? 'opacity-0' : 'opacity-100'
+                    )}
+                >
+                    {user.fullName && user.imageUrl ? (
+                        <>
+                            <Avatar className="size-7">
+                                <AvatarImage src={user.imageUrl} />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <div className="text-sm font-normal">{user.fullName}</div>
+                        </>
+                    ) : (
+                        <>
+                            <Skeleton className="size-7 min-w-7 rounded-full" />
+                            <Skeleton className="h-5 w-2/3 rounded-full" />
+                        </>
+                    )}
+                </div>
                 <Unauthenticated>
                     <SidebarMenuButton size="lg" className="cursor-pointer" asChild>
                         <SignInButton />
                     </SidebarMenuButton>
                 </Unauthenticated>
-                <AuthLoading>
-                    <div className="pointer-events-none top-0 flex items-center gap-2 p-2 opacity-50">
-                        {user ? (
-                            <>
-                                <Avatar className="size-7">
-                                    <AvatarImage src={user.imageUrl} />
-                                    <AvatarFallback>CN</AvatarFallback>
-                                </Avatar>
-                                <div className="text-sm">{user.fullName}</div>
-                            </>
-                        ) : (
-                            <>
-                                <Skeleton className="size-7 min-w-7 rounded-full" />
-                                <Skeleton className="h-5 w-2/3 rounded-full" />
-                            </>
-                        )}
-                    </div>
-                </AuthLoading>
             </SidebarFooter>
         </Sidebar>
     )
