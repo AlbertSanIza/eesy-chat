36588274a304a8d5de341ed02b150d926d160ca3
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-14 15:37:09 -0700

    Fetch and display messages for the selected thread in the route component

--- src/routes/$threadId.tsx
+++ src/routes/$threadId.tsx
@@ -1,7 +1,9 @@
 import { createFileRoute, useParams } from '@tanstack/react-router'
+import { useQuery } from 'convex/react'
 
 import { Messages } from '@/components/messages'
 import { useSidebar } from '@/components/ui/sidebar'
+import { api } from '@/convex/_generated/api'
 import type { Id } from '@/convex/_generated/dataModel'
 import { useStore } from '@/lib/store'
 import { cn } from '@/lib/utils'
@@ -14,6 +16,7 @@ function RouteComponent() {
     const { open, isMobile } = useSidebar()
     const { threadId } = useParams({ from: Route.fullPath })
     const thread = useStore(({ threads }) => threads.find((thread) => thread._id === threadId))
+    const messages = useQuery(api.messages.findAll, { threadId: threadId as Id<'threads'> })
 
     if (!thread) {
         return 'Loading...'
@@ -22,6 +25,7 @@ function RouteComponent() {
     return (
         <div className={cn('w-full pt-14 pb-33 sm:pt-8', open && !isMobile && 'max-w-[calc(100vw-var(--sidebar-width))]')}>
             <div className="mx-auto flex max-w-4xl flex-col gap-6 px-6 sm:px-14">
+                <div className="whitespace-pre-wrap">{JSON.stringify(messages)}</div>
                 <Messages threadId={threadId as Id<'threads'>} />
             </div>
         </div>
