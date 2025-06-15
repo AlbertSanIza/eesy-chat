47147c08638448e26c2c429071fd199f9588df62
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-15 16:59:28 -0700

    Add findOne query to retrieve a model by ID

--- src/convex/models.ts
+++ src/convex/models.ts
@@ -1,3 +1,5 @@
+import { v } from 'convex/values'
+
 import { query } from './_generated/server'
 
 export const findAll = query({
@@ -10,4 +12,9 @@ export const findAll = query({
             .order('asc')
             .collect()
     }
+})
+
+export const findOne = query({
+    args: { id: v.id('models') },
+    handler: async (ctx, { id }) => await ctx.db.get(id)
 })
