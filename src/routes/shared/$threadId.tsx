73990f30ff9eb7b1a5dd7cbb8dc6bc3be86fbc39
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-14 17:16:18 -0700

    Replace placeholder message with ThemeToggle component in $threadId route

--- src/routes/shared/$threadId.tsx
+++ src/routes/shared/$threadId.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { ThemeToggle } from '@/components/theme/toggle'
+
 export const Route = createFileRoute('/shared/$threadId')({
     component: RouteComponent
 })
 
 function RouteComponent() {
-    return <div>Hello "/shared"!</div>
+    return (
+        <>
+            <ThemeToggle />
+        </>
+    )
 }
