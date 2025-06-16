fa5604099e0dafca10facc19533d1d4d03dee020
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-16 16:53:19 -0700

    Fix import name for TanStack Router in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import tailwindcss from '@tailwindcss/vite'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [tailwindcss(), TanStackRouterVite({ target: 'react', autoCodeSplitting: true, generatedRouteTree: './src/lib/route-tree.gen.ts' }), react()],
+    plugins: [tailwindcss(), tanstackRouter({ target: 'react', autoCodeSplitting: true, generatedRouteTree: './src/lib/route-tree.gen.ts' }), react()],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src')
