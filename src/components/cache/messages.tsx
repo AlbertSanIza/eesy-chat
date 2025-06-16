c1b2fccf2cfae3537eab5a2afd137f11476f10a1
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-15 18:35:55 -0700

    Refactor MessagesCache to use UserMessageCache for improved structure and clarity

--- src/components/cache/messages.tsx
+++ src/components/cache/messages.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from 'convex/react'
+import { Fragment } from 'react'
 
 import { api } from '@/convex/_generated/api'
 import type { Id } from '@/convex/_generated/dataModel'
@@ -7,10 +8,25 @@ import { useStore } from '@/lib/store'
 export function MessagesCache() {
     const { threads } = useStore()
 
-    return threads.slice(0, 20).map((thread) => <MessageCache key={thread._id} id={thread._id} />)
+    return threads.slice(0, 20).map((thread) => (
+        <Fragment key={thread._id}>
+            <UserMessageCache id={thread._id} />
+        </Fragment>
+    ))
 }
 
-function MessageCache({ id }: { id: Id<'threads'> }) {
-    useQuery(api.messages.findAll, { threadId: id })
+function UserMessageCache({ id }: { id: Id<'threads'> }) {
+    const messages = useQuery(api.messages.findAll, { threadId: id })
+
+    return messages?.map((message) => (
+        <Fragment key={message._id}>
+            <AssistantMessageCache key={message._id} id={message._id} />
+        </Fragment>
+    ))
+}
+
+function AssistantMessageCache({ id }: { id: Id<'messages'> }) {
+    useQuery(api.messages.body, { messageId: id })
+
     return null
 }
