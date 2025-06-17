0b758e1bb666151d45aeedc6e97f7425790c452b
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-17 00:12:31 -0700

    Remove redundant VITE_CONVEX_URL check in getConvexSiteUrl function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -20,10 +20,6 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getConvexSiteUrl() {
-    const VITE_CONVEX_URL = import.meta.env.VITE_CONVEX_URL
-    if (!VITE_CONVEX_URL) {
-        throw new Error('VITE_CONVEX_URL is not defined in the environment variables.')
-    }
     let convexSiteUrl
     if (VITE_CONVEX_URL.includes('.cloud')) {
         convexSiteUrl = VITE_CONVEX_URL.replace(/\.cloud$/, '.site')
