9db2c7d68df9472a7b6d31be6e080fb82c03a9ab
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-17 16:56:04 -0700

    Add highlightCode invocation in Cache component for syntax highlighting

--- src/components/cache/index.tsx
+++ src/components/cache/index.tsx
@@ -2,11 +2,13 @@ import { MessagesCache } from '@/components/cache/messages'
 import { useModelsCache } from '@/hooks/use-models-cache'
 import { useThreadsCache } from '@/hooks/use-threads-cache'
 import { useUserCache } from '@/hooks/use-user-cache'
+import { highlightCode } from '@/lib/shiki'
 
 export function Cache() {
     useUserCache()
     useThreadsCache()
     useModelsCache()
+    highlightCode('', 'text')
 
     return <MessagesCache />
 }
