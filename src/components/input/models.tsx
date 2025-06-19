9aa0f2a391345fa87bd3a2e7d636cbe0b4947e93
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-19 01:54:12 -0700

    Refactor InputModelsSelect component to improve model filtering logic and enhance readability

--- src/components/input/models.tsx
+++ src/components/input/models.tsx
@@ -4,45 +4,62 @@ import { ChevronDownIcon } from 'lucide-react'
 import { useStore } from '@/lib/zustand/store'
 
 export function InputModelsSelect() {
+    const key = useStore((state) => state.key)
     const model = useStore((state) => state.model)
     const models = useStore((state) => state.models)
     const { threadId } = useParams({ strict: false })
     const setModel = useStore((state) => state.setModel)
     const thread = useStore((state) => state.threads.find((thread) => thread._id === threadId))
 
-    const availableModels = models.filter((model) => {
-        if (!thread || (thread.type === 'text' && model.model !== 'dall-e-3' && model.model !== 'eleven_monolingual_v2')) {
+    const availableModels = models
+        .filter((model) => {
+            if (!thread || (thread.type === 'text' && model.model !== 'dall-e-3' && model.model !== 'eleven_monolingual_v2')) {
+                return true
+            }
+            if (thread.type === 'image' && model.model === 'dall-e-3') {
+                return true
+            }
+            if (thread.type === 'sound' && model.model === 'eleven_monolingual_v2') {
+                return true
+            }
+            return false
+        })
+        .filter((model) => {
+            if (model.withKey) {
+                if (model.service === 'openRouter' && !key.openRouter) {
+                    return false
+                }
+                if (model.service === 'openAi' && !key.openAi) {
+                    return false
+                }
+                if (model.service === 'elevenLabs' && !key.elevenLabs) {
+                    return false
+                }
+            }
             return true
-        }
-        if (thread.type === 'image' && model.model === 'dall-e-3') {
-            return true
-        }
-        if (thread.type === 'sound' && model.model === 'eleven_monolingual_v2') {
-            return true
-        }
-        return false
-    })
+        })
 
     return (
-        <div className="grid grid-cols-1 text-sm">
-            <select
-                className="col-start-1 row-start-1 h-9 cursor-pointer appearance-none rounded-md border bg-background pr-7 pl-2 shadow-xs outline-none hover:bg-accent hover:text-accent-foreground"
-                value={model?._id}
-                onChange={(event) => {
-                    const modelId = event.target.value
-                    const model = models.find((m) => m._id === modelId)
-                    if (model) {
-                        setModel(model)
-                    }
-                }}
-            >
-                {availableModels.map((model) => (
-                    <option key={model._id} value={model._id}>
-                        {model.provider}: {model.label}
-                    </option>
-                ))}
-            </select>
-            <ChevronDownIcon aria-hidden="true" className="pointer-events-none col-start-1 row-start-1 mr-2 self-center justify-self-end sm:size-4" />
-        </div>
+        !!availableModels.length && (
+            <div className="grid grid-cols-1 text-sm">
+                <select
+                    className="col-start-1 row-start-1 h-9 cursor-pointer appearance-none rounded-md border bg-background pr-7 pl-2 shadow-xs outline-none hover:bg-accent hover:text-accent-foreground"
+                    value={model?._id}
+                    onChange={(event) => {
+                        const model = models.find((m) => m._id === event.target.value)
+                        if (model) {
+                            setModel(model)
+                        }
+                    }}
+                >
+                    {availableModels.map((model) => (
+                        <option key={model._id} value={model._id}>
+                            {model.provider}: {model.label}
+                        </option>
+                    ))}
+                </select>
+                <ChevronDownIcon aria-hidden="true" className="pointer-events-none col-start-1 row-start-1 mr-2 self-center justify-self-end sm:size-4" />
+            </div>
+        )
     )
 }
