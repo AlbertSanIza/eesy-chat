75dc0dcc02516274f060541af24ce956d62dfa8f
Albert Sanchez <albert.san.iza@gmail.com>
2025-06-21 22:37:38 -0700

    Fix echo message for non-production builds in vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -3,6 +3,6 @@ if [[ "$VERCEL_ENV" == "production" ]]; then
   echo "Building for Production"
   bunx convex deploy --cmd 'bun run build'
 else
-  echo "Building for Staging"
+  echo "Building for Development"
   bun run build
 fi
