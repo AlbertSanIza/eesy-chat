cd61579d156bb0ab3f187e8e4a81eb85dc80ee72
Albert Sanchez <albert.san.iza@gmail.com>
2025-07-06 10:29:07 -0700

    Prevent Key interaction if not signed in (#6)

--- src/components/cache/undo.tsx
+++ src/components/cache/undo.tsx
@@ -3,12 +3,16 @@ import { useEffect } from 'react'
 
 export function UndoCache() {
     const setUser = useStore((state) => state.setUser)
+    const setModels = useStore((state) => state.setModels)
     const setThreads = useStore((state) => state.setThreads)
+    const setThreadSearch = useStore((state) => state.setThreadSearch)
 
     useEffect(() => {
         setUser({ isSignedIn: false })
+        setModels([])
         setThreads([])
-    }, [setUser, setThreads])
+        setThreadSearch('')
+    }, [setModels, setThreads, setUser, setThreadSearch])
 
     return null
 }
